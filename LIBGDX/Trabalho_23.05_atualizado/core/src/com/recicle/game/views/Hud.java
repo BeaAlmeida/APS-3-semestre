f59abefd3a0f46b00905fad2c538f39594aacc8f
Beatriz Almeida <BeaAlmeida@users.noreply.github.com>
2020-05-23 20:41:47 -0300

    23.05 att

---
+++ LIBGDX/Trabalho_23.05_atualizado/core/src/com/recicle/game/views/Hud.java
@@ -0,0 +1,92 @@
+package com.recicle.game.views;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.Disposable;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.recicle.game.MainClass;
+
+public class Hud implements Disposable {
+    private MainClass parent;
+    private ExtendViewport viewport;
+    public Stage stage;
+
+    public static int worldTimer;
+    private float timeCount;
+    private static int score;
+
+    public static final int TIME = 15;
+
+    private Label countdownLabel;
+    private static Label scoreLabel;
+
+    public Hud(MainClass mainClass, SpriteBatch batch) {
+        parent = mainClass;
+        worldTimer = TIME;
+        timeCount = 0;
+        score = 0;
+
+        viewport = new ExtendViewport(200, 200, new OrthographicCamera());
+        stage = new Stage(viewport, batch);
+
+        Table table = new Table();
+        table.top();
+        table.setFillParent(true);
+
+        countdownLabel = new Label(String.format("%03d", worldTimer), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        scoreLabel = new Label(String.format("%06d", score), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+
+        table.add(countdownLabel).expandX().padTop(5);
+        table.add(scoreLabel).expandX().padTop(5);
+
+        table.row();
+        table.add(scoreLabel).expandX();
+        table.add(countdownLabel).expandX();
+
+        stage.addActor(table);
+
+    }
+
+    public static void addScore(){
+        score +=  10;
+        worldTimer += 2;
+
+    }
+    public static void subScore(){
+        score -= 10;
+        worldTimer -= 2;
+    }
+
+    public void update(float dt) {
+        timeCount += dt;
+        if (timeCount >= 1) {
+            if (worldTimer > 0) {
+                worldTimer--;
+                countdownLabel.setText(String.format("%03d", worldTimer));
+                timeCount = 0;
+            }
+        }
+        if (worldTimer <= 0){
+            System.out.println("Time out!");
+            parent.changeScreen(MainClass.MENU);
+            worldTimer = TIME;
+            score = 0;
+        }
+
+        if(score < 0) score = 0;
+
+        scoreLabel.setText(String.format("%06d", score));
+
+    }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+    }
+
+}
